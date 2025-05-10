e3927d267d54b2c4e7001f5eb04a24281edd5347
Gino Fabrizio <51064212+SirTortuga333@users.noreply.github.com>
2025-05-10 19:56:17 -0300

    Created basic node class

---
+++ node.js
@@ -0,0 +1,17 @@
+export class Node {
+  value = null;
+  left = null;
+  right = null;
+
+  constructor(value) {
+    this.value = value;
+  }
+
+  setLeft(node) {
+    this.left = node;
+  }
+
+  setRight() {
+    this.right = node;
+  }
+}
